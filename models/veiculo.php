46036de9b124b4e279309938f54d6c6727fbd83f
YasBorba <yasmin.b0rbalopes@gmail.com>
2025-04-28 11:26:26 -0300

    commit

--- models/veiculo.php
+++ models/veiculo.php
@@ -8,8 +8,25 @@ abstract class Veiculo {
     protected string $placa;
     protected bool $disponivel;
 
-    public function __construct(string model
-    {
-        
+    public function __construct(string $modelo, string $placa){
+        $this -> modelo = $modelo;
+        $this -> placa = $placa;
+        $this -> disponivel = true;
+    }
+    
+    // Funçao para calculo de aluguel
+    abstract public function calcularAluguel(int $dias) : float;
+
+    public function isDisponivel(): bool{
+        return $this->disponivel;
+    }
+    public function getModelo(): string {
+        return $this->modelo;
+    }
+    public function getPlaca(): string{
+        return $this->placa;
+    }
+    public function setDisponivel(bool $disponivel):void{
+        $this ->disponivel = $disponivel;
     }
 }
